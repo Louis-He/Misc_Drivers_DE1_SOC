0d644039322562fb0f9d47e5f757d8c9594d5159
Louis-He <siweihe.eng@gmail.com>
2024-09-18 22:27:18 -0400

    organize file structure

--- Lab2/src/part1/part1.c
+++ Lab2/src/part1/part1.c
@@ -11,22 +11,22 @@ int open_physical (int);
 void * map_physical (int, unsigned int, unsigned int);
 void close_physical (int);
 int unmap_physical (void *, unsigned int);
-
 // used to exit the program cleanly
-volatile sig_atomic_t stop;
+volatile sig_atomic_t stop = 0;
 void catchSIGINT(int);
 
+
 char  MESSAGE[] = "     Intel SoC FPGA      ";
-unsigned char  SEGMENT_MESSAGE[] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x10, 0x54, 0x78, 0x79, 0x38, 0x0,
+unsigned char  SEGMENT_MESSAGE[] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x04, 0x54, 0x78, 0x79, 0x38, 0x0,
                                         0x6D, 0x5C, 0x39, 0x0,
                                         0x71, 0x73, 0x7D, 0x77, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
 unsigned lowest_message_idx = 5;
+
 
 int main(void)
 {
 	int fd = -1;
 	fd = open_physical(fd);
-
 	void* base_ptr = map_physical(fd, LW_BRIDGE_BASE, LW_BRIDGE_SPAN);
 
 	volatile unsigned int * KEY_ptr = (unsigned int *) (base_ptr + KEY_BASE + 0xC);
@@ -35,51 +35,38 @@ int main(void)
 	unsigned int * HEX3_0_ptr = (unsigned int *) (base_ptr + HEX3_HEX0_BASE);
 	unsigned int * HEX5_4_ptr = (unsigned int *) (base_ptr + HEX5_HEX4_BASE);
 
-	int stop = 0;
+	printf("\e[2J\e[10;10H----------\e[12;10H----------\n");
+    fflush(stdout);
 
 	while (1) {
-		// KEY
-		unsigned int edge_reg = (*KEY_ptr & 0xF);
-		if (edge_reg != 0) {
-			stop = stop ^ 0x1;
-			*KEY_ptr = edge_reg;  // Clear the interrupt
-		}
-
-
 		// HEX
 		*(HEX3_0_ptr) = SEGMENT_MESSAGE[lowest_message_idx+5] | SEGMENT_MESSAGE[lowest_message_idx+4] << 8 |
 		SEGMENT_MESSAGE[lowest_message_idx+3] << 16  | SEGMENT_MESSAGE[lowest_message_idx+2] << 24;
 		*(HEX5_4_ptr) = SEGMENT_MESSAGE[lowest_message_idx+1] | SEGMENT_MESSAGE[lowest_message_idx] << 8;
 
-
-		// Terminal
-		printf("\e[2J");
-		fflush(stdout);
-		printf("\e[10;10H");
-		fflush(stdout);
-		printf("----------");
-		fflush(stdout);
-		printf("\e[11;10H");
-		fflush(stdout);
-		printf("| %c%c%c%c%c%c |", MESSAGE[lowest_message_idx], MESSAGE[lowest_message_idx+1], 
-				MESSAGE[lowest_message_idx+2], MESSAGE[lowest_message_idx+3], MESSAGE[lowest_message_idx+4], MESSAGE[lowest_message_idx+5]);
-		fflush(stdout);
-		printf("\e[12;10H");
-		fflush(stdout);
-		printf("----------");
-		fflush(stdout);
+        printf("\e[11;10H| %c%c%c%c%c%c |",
+               MESSAGE[lowest_message_idx], MESSAGE[lowest_message_idx+1], 
+               MESSAGE[lowest_message_idx+2], MESSAGE[lowest_message_idx+3], 
+               MESSAGE[lowest_message_idx+4], MESSAGE[lowest_message_idx+5]);
+        fflush(stdout);
 
 		struct timespec ts;
 		ts.tv_sec = 0;										// used to delay
 		ts.tv_nsec = 200000000;								// 10^8 ns = 0.1 sec
 		nanosleep (&ts, NULL);
-		if (!stop) {
-			lowest_message_idx++;
+
+
+		// KEY
+		unsigned int edge_reg = *KEY_ptr & 0xF;
+		if (edge_reg != 0) {
+			stop ^= 0x1; 
+			*KEY_ptr = edge_reg;  // Clear the interrupt
 		}
 
-		if (lowest_message_idx > 19) {
-			lowest_message_idx = 0;
+		if (!stop) {
+			lowest_message_idx = (lowest_message_idx + 1) % 20;
 		}
+
 	}
 	
 
