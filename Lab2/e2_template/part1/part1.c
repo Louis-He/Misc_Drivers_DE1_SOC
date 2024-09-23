955a753844e82d906f324723fd3af75ac4f7446a
Louis-He <siweihe.eng@gmail.com>
2024-09-23 13:47:56 -0400

    complete Lab2, complete Lab3/part1

--- Lab2/e2_template/part1/part1.c
+++ Lab2/e2_template/part1/part1.c
@@ -1,11 +1,87 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/mman.h>
 #include <signal.h>
+#include <fcntl.h>
 #include <time.h>
-#include "address_map_arm.h"
-#include "physical.h"
+#include <sys/mman.h>
+#include "../include/address_map_arm.h"
+
+/* Prototypes for functions used to access physical memory addresses */
+int open_physical (int);
+void * map_physical (int, unsigned int, unsigned int);
+void close_physical (int);
+int unmap_physical (void *, unsigned int);
+// used to exit the program cleanly
+volatile sig_atomic_t stop = 0;
+void catchSIGINT(int);
 
 
-/**  your part 1 user code here  **/
+char  MESSAGE[] = "     Intel SoC FPGA      ";
+unsigned char  SEGMENT_MESSAGE[] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x04, 0x54, 0x78, 0x79, 0x38, 0x0,
+                                        0x6D, 0x5C, 0x39, 0x0,
+                                        0x71, 0x73, 0x7D, 0x77, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+unsigned lowest_message_idx = 5;
+
+
+int main(void)
+{
+	int fd = -1;
+	fd = open_physical(fd);
+	void* base_ptr = map_physical(fd, LW_BRIDGE_BASE, LW_BRIDGE_SPAN);
+
+	volatile unsigned int * KEY_ptr = (unsigned int *) (base_ptr + KEY_BASE + 0xC);
+	*KEY_ptr = 0xF;
+
+	unsigned int * HEX3_0_ptr = (unsigned int *) (base_ptr + HEX3_HEX0_BASE);
+	unsigned int * HEX5_4_ptr = (unsigned int *) (base_ptr + HEX5_HEX4_BASE);
+
+	printf("\e[2J\e[10;10H----------\e[12;10H----------\n");
+    fflush(stdout);
+
+	while (1) {
+		// HEX
+		*(HEX3_0_ptr) = SEGMENT_MESSAGE[lowest_message_idx+5] | SEGMENT_MESSAGE[lowest_message_idx+4] << 8 |
+		SEGMENT_MESSAGE[lowest_message_idx+3] << 16  | SEGMENT_MESSAGE[lowest_message_idx+2] << 24;
+		*(HEX5_4_ptr) = SEGMENT_MESSAGE[lowest_message_idx+1] | SEGMENT_MESSAGE[lowest_message_idx] << 8;
+
+        printf("\e[11;10H| %c%c%c%c%c%c |",
+               MESSAGE[lowest_message_idx], MESSAGE[lowest_message_idx+1], 
+               MESSAGE[lowest_message_idx+2], MESSAGE[lowest_message_idx+3], 
+               MESSAGE[lowest_message_idx+4], MESSAGE[lowest_message_idx+5]);
+        fflush(stdout);
+
+		struct timespec ts;
+		ts.tv_sec = 0;										// used to delay
+		ts.tv_nsec = 200000000;								// 10^8 ns = 0.1 sec
+		nanosleep (&ts, NULL);
+
+
+		// KEY
+		unsigned int edge_reg = *KEY_ptr & 0xF;
+		if (edge_reg != 0) {
+			stop ^= 0x1; 
+			*KEY_ptr = edge_reg;  // Clear the interrupt
+		}
+
+		if (!stop) {
+			lowest_message_idx = (lowest_message_idx + 1) % 20;
+		}
+
+	}
+	
+
+	// EXIT
+	*(HEX3_0_ptr) = 0;
+	*(HEX5_4_ptr) = 0;
+	unmap_physical (base_ptr, LW_BRIDGE_SPAN);	// release the physical-memory mapping
+	close_physical (fd);	// close /dev/mem
+	printf ("\nExiting program\n");
+	return 0;
+}
+
+
+/* Function to allow clean exit of the program */
+void catchSIGINT(int signum)
+{
+	stop = 1;
+}
--- Lab3/e3_template/part2/KEY_SW.c
+++ Lab3/e3_template/part2/KEY_SW.c
@@ -6,4 +6,182 @@
 #include <asm/io.h>                 // for mmap
 #include "../include/address_map_arm_vm.h"
 
-/**  your part 2 kernel code here  **/
+static int device_open (struct inode *, struct file *);
+static int device_release (struct inode *, struct file *);
+static ssize_t device_read (struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+
+static ssize_t device_sw_read(struct file *, char *, size_t, loff_t *);
+
+static struct file_operations key_dev_fops = {
+    .owner = THIS_MODULE,
+    .read = device_read,
+    .write = device_write,
+    .open = device_open,
+    .release = device_release
+};
+
+static struct file_operations sw_dev_fops = {
+    .owner = THIS_MODULE,
+    .read = device_sw_read,
+    .write = device_write,
+    .open = device_open,
+    .release = device_release
+};
+
+
+#define SUCCESS 0
+#define KEY_DEV_NAME "KEY"
+#define SW_DEV_NAME "SW"
+
+void * LW_virtual;             // used to map physical addresses for the light-weight bridge
+volatile int* KEY_ptr;
+volatile int* SW_ptr;
+
+static struct miscdevice key_dev = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = KEY_DEV_NAME,
+    .fops = &key_dev_fops,
+    .mode = 0666
+};
+static struct miscdevice sw_dev = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = SW_DEV_NAME,
+    .fops = &sw_dev_fops,
+    .mode = 0666
+};
+static int key_dev_registered = 0;
+static int sw_dev_registered = 0;
+
+static int __init start_key_swdev(void)
+{
+    // generate a virtual address for the FPGA lightweight bridge
+    LW_virtual = ioremap_nocache (LW_BRIDGE_BASE, LW_BRIDGE_SPAN);
+    
+    int err = misc_register (&key_dev);
+    if (err < 0) {
+        printk (KERN_ERR "/dev/%s: misc_register() failed\n", KEY_DEV_NAME);
+    }
+    else {
+        printk (KERN_INFO "/dev/%s driver registered\n", KEY_DEV_NAME);
+        key_dev_registered = 1;
+
+        KEY_ptr = LW_virtual + KEY_BASE;
+
+        *(KEY_ptr + 3) = 0xF; 
+    }
+
+    err = misc_register (&sw_dev);
+    if (err < 0) {
+        printk (KERN_ERR "/dev/%s: misc_register() failed\n", SW_DEV_NAME);
+    } else {
+        printk (KERN_INFO "/dev/%s driver registered\n", SW_DEV_NAME);
+        sw_dev_registered = 1;
+        SW_ptr = LW_virtual + SW_BASE;
+    }
+
+    return err;
+}
+
+static void __exit stop_key_swdev(void)
+{
+    iounmap (LW_virtual);
+    if (key_dev_registered) {
+        misc_deregister (&key_dev);
+        printk (KERN_INFO "/dev/%s driver de-registered\n", KEY_DEV_NAME);
+
+        KEY_ptr = NULL;
+    }
+
+    if (sw_dev_registered) {
+        misc_deregister (&sw_dev);
+        printk (KERN_INFO "/dev/%s driver de-registered\n", SW_DEV_NAME);
+
+        SW_ptr = NULL;
+    }
+}
+
+static int device_open(struct inode *inode, struct file *file)
+{
+    printk(KERN_INFO "Opened device: %s\n", KEY_DEV_NAME);
+    return SUCCESS;
+}
+
+static int device_release(struct inode *inode, struct file *file)
+{
+    printk(KERN_INFO "Closed device: %s\n", KEY_DEV_NAME);
+    return 0;
+}
+
+static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t *offset)
+{
+    if (*offset == 2) {
+        return 0;
+    }
+
+    printk(KERN_INFO "Reading from device: %s\n", KEY_DEV_NAME);
+    char key_value[2];
+
+    key_value[0] = *(KEY_ptr + 3);
+
+    // clear edge register
+    *(KEY_ptr + 3) = key_value[0]; 
+
+    if (key_value[0] < 10) {
+        key_value[0] += '0';
+    } else {
+        key_value[0] = 'A' + key_value[0] - 10;
+    }
+
+    key_value[1] = '\n';
+
+    if (copy_to_user (buffer, key_value, 2) != 0)
+    {
+        printk(KERN_ERR "Error: copy_to_user unsuccessful\n");
+    }
+
+    printk(KERN_INFO "Key value: %c\n", key_value);
+    *offset = 2;
+    return 2;
+}
+
+static ssize_t device_sw_read(struct file *filp, char *buffer, size_t length, loff_t *offset)
+{
+    if (*offset == 4) {
+        return 0;
+    }
+
+    printk(KERN_INFO "Reading from device: %s\n", SW_DEV_NAME);
+    char sw_value[4];
+    memset(sw_value, 0, 4);
+
+    int sw_read_value = *SW_ptr;
+    sprintf(sw_value, "%x", sw_read_value);
+
+    sw_value[3] = '\n';
+
+    if (copy_to_user (buffer, sw_value, 4) != 0)
+    {
+        printk(KERN_ERR "Error: copy_to_user unsuccessful\n");
+    }
+
+    printk(KERN_INFO "Switch value: %x\n", sw_read_value);
+    *offset = 4;
+    return 4;
+}
+
+/* Called when a process writes to key_swdev. Stores the data received into key_swdev_msg, and 
+ * returns the number of bytes stored. */
+static ssize_t device_write(struct file *filp, const char *buffer, size_t length, loff_t *offset)
+{
+    size_t bytes;
+
+    bytes = 0;
+
+    return bytes;
+}
+
+
+MODULE_LICENSE("GPL");
+module_init (start_key_swdev);
+module_exit (stop_key_swdev);
